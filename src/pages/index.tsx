98fe016a5acad85c21cb88bfc44aa9e345ba803b
benotargiacomo <benotargiacomo@gmail.com>
2022-06-24 19:14:36 -0300

    db connection test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+import { prisma } from  '../db/client';
+
+const Home: NextPage = (props: any) => {
   return (
-    <h1>Hello World!</h1>
+    <h1>{ props.user.username }</h1>
   );
+}
+
+export const getServerSideProps = async () => {
+  const user = await prisma.user.findFirst({
+    where: {
+      username: 'Bernardo',
+    }
+  });
+
+  return {
+    props: {
+      user,
+    }
+  }
 }
 
 export default Home
