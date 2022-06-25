6cb9d7913c6ca3a31dd37381e2076921538783ec
benotargiacomo <benotargiacomo@gmail.com>
2022-06-24 21:27:30 -0300

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
 import type { NextPage } from 'next';
 
 import { prisma } from  '../db/client';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = (props: any) => {
+  const { data, isLoading } = trpc.useQuery(['hello']);
+
+  if (isLoading || !data) return <h1>Loading...</h1>
+  
   return (
-    <h1>{ props.user.username }</h1>
+    <h1>{ data.greeting }</h1>
   );
 }
 
-export const getServerSideProps = async () => {
-  const user = await prisma.user.findFirst({
-    where: {
-      username: 'Bernardo',
-    }
-  });
+// export const getServerSideProps = async () => {
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       username: 'Bernardo',
+//     }
+//   });
 
-  return {
-    props: {
-      user,
-    }
-  }
-}
+//   return {
+//     props: {
+//       user,
+//     }
+//   }
+// }
 
 export default Home
