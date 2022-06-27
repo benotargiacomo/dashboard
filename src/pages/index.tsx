e15d0269cf1849eefe326a62d06c9ece2c270aaf
benotargiacomo <benotargiacomo@gmail.com>
2022-06-27 18:59:13 -0300

    redirect middleware

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next';
-
+import React from 'react';
 import { trpc } from '@/utils/trpc';
 
-const Home: NextPage = () => {
+const Home: React.FC = () => {
   const { data, isLoading } = trpc.useQuery(['work.getAll']);
   
   if (isLoading || !data) return <h1>Loading...</h1>;
@@ -11,7 +10,7 @@ const Home: NextPage = () => {
     <>
       <li>
         {data?.map((work) => (
-          <ul key={work.id}>
+          <ul className="flex flex-col gap-2" key={work.id}>
             <span>{work.id}</span>
             <span>{work.name}</span>
             <span>{work.description}</span>
