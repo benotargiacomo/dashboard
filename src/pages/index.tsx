d01450125164e7c55dffb21a137faeeb1a87335e
benotargiacomo <benotargiacomo@gmail.com>
2022-06-27 16:44:13 -0300

    querys work route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,26 @@ import { trpc } from '@/utils/trpc';
 
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(['work.getAll']);
-
+  
   if (isLoading || !data) return <h1>Loading...</h1>;
 
-  return <h1>{data.length}</h1>;
+  return (
+    <>
+      <li>
+        {data?.map((work) => (
+          <ul key={work.id}>
+            <span>{work.id}</span>
+            <span>{work.name}</span>
+            <span>{work.description}</span>
+            <span>{work.repository}</span>
+            <span>{work.deploy}</span>
+            <span>{work.thumbnail}</span>
+            <span>{ JSON.stringify(work.tags, null, 2)}</span>
+          </ul>
+        ))}
+      </li>
+    </>
+  );
 };
 
 export default Home;
--- src/server/router/works.ts
+++ src/server/router/works.ts
@@ -3,25 +3,41 @@ import { z } from 'zod';
 
 import { prisma } from '@/db/client';
 
-// ROTAS PARA CRIAR
-// - um find que inclua as letras do nome - Search Bar
-// - um update
 export const workRouter = trpc.router()
   .query('getAll', {
     async resolve() {
-      const works = await prisma.project.findMany();
+      const works = await prisma.project.findMany({
+        include: {
+          tags: {
+            select: {
+              name: true,
+            }
+          }
+        },
+      });
 
       return works;
     }
   })
-  .query("getById", {
-    input: z.object({ id: z.string().cuid({ message: "Invalid ID" })}),
+  .query('getById', {
+    input: z.object({
+      id: z.string().cuid({ message: "Invalid ID" })
+    }),
     async resolve({ input }) {
       const work = await prisma.project.findUnique({
         where: {
           id: input.id
-        }
+        },
+        include: {
+          tags: {
+            select: {
+              name: true,
+            }
+          }
+        },
       })
+      
+      return work;
     }
   })
   .mutation('create', {
@@ -51,9 +67,48 @@ export const workRouter = trpc.router()
           }
         }
       })
+
+      return project.id;
     }
   })
-  .mutation("delete", {
+  .mutation('edit', {
+    input: z.object({
+      id: z.string().cuid({ message: "Invalid ID" }),
+      data: z.object({
+        name: z.string().min(5, { message: "Must be 5 or more characters long" }),
+        description: z.string().min(5, { message: "Must be 5 or more characters long" }).optional(),
+        repository: z.string().url({ message: "Invalid url" }).optional(),
+        deploy: z.string().url({ message: "Invalid url" }).optional(),
+        thumbnail: z.string().url({ message: "Invalid url" }).optional(),
+        tags: z.array(z.string()).optional(),
+      })
+    }),
+    async resolve({ input }) {
+      const updated = await prisma.project.update({
+        where: {
+          id: input.id
+        },
+        data: {
+          name: input.data.name, 
+          description: input.data.description,
+          repository: input.data.repository,
+          deploy: input.data.deploy,
+          thumbnail: input.data.thumbnail,
+          tags: {
+            connectOrCreate: input.data.tags?.map((tag) => {
+              return {
+                create: { name: tag },
+                where: { name: tag }
+              }
+            })
+          }
+        }
+      })
+
+      return updated;
+    }
+  })
+  .mutation('delete', {
     input: z.object({ id: z.string().cuid({ message: "Invalid ID" })}),
     async resolve({ input }) {
       const work = await prisma.project.delete({
@@ -61,6 +116,8 @@ export const workRouter = trpc.router()
           id: input.id
         }
       })
+
+      return work;
     }
   });
 
