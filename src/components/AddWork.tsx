8d83c36a96c59f02104f03ceb06235d8213758aa
benotargiacomo <benotargiacomo@gmail.com>
2022-07-01 00:07:38 -0300

    form to addWork

---
+++ src/components/AddWork.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { trpc } from '@/utils/trpc';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { Error } from '@/components/Error';
+
+const addSchema = z.object({
+  name: z.string()
+    .min(1, { message: 'Name is required' })
+    .max(50, { message: 'Name must contain at most 40 character(s)'}),
+  description: z.string()
+    .min(1, { message: 'Description is required' })
+    .max(200, { message: 'Description must contain at most 200 character(s)'}),
+  repository: z.string()
+    .min(1, { message: 'Repository is required' })
+    .url({ message: 'Invalid url' }),
+  deploy: z.string()
+    .min(1, { message: 'Deploy is required' })
+    .url({ message: 'Invalid url' }),
+  thumbnail: z.string()
+    .min(1, { message: 'Thumbnail is required' })
+    .url({ message: 'Invalid url' }),
+  tags: z.string()
+    .min(1, { message: 'Tags is required' }),
+})
+
+type Inputs = z.infer<typeof addSchema>;
+
+interface AddProps {
+	addProjectToggle: () => void
+}
+
+export const AddWork: React.FC<AddProps> = ({ addProjectToggle }) => {
+  const utils = trpc.useContext();
+  const { mutateAsync } = trpc.useMutation('work.create', {
+    async onSuccess() {
+      await utils.invalidateQueries(['work.getAll']);
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<Inputs>({
+		defaultValues: {
+      name: '',
+      description: '',
+      repository: '',
+      deploy: '',
+      thumbnail: '',
+      tags: '',
+    },
+    resolver: zodResolver(addSchema),
+    // Validation DEBUG!!!
+    // resolver: async (data, context, options) => {
+    //   console.log('formData', data)
+    //   console.log('validation result', await zodResolver(addSchema)(data, context, options))
+    //   return zodResolver(addSchema)(data, context, options)
+    // },
+  });
+
+  const addProject = (work: Inputs): void => {
+    mutateAsync({
+      name: work.name,
+      description: work.description,
+      repository: work.repository,
+      deploy: work.deploy,
+      thumbnail: work.thumbnail,
+      tags: work.tags.split(', '),
+    });
+  };
+
+  useEffect(() => {
+    reset();
+				
+		addProjectToggle();
+  }, [isSubmitSuccessful, reset, addProjectToggle]);
+
+  return (
+		<div>
+			<button type="button" onClick={ () => addProjectToggle() }>x</button>
+			<form onSubmit={handleSubmit(addProject)} className="flex flex-col gap-8">
+				<input {...register('name')} placeholder="Name" />
+				<Error>{errors.name?.message}</Error>
+				<input {...register('description')} placeholder="Description" />
+				<Error>{errors.description?.message}</Error>
+				<input {...register('repository')} placeholder="Repository" />
+				<Error>{errors.repository?.message}</Error>
+				<input {...register('deploy')} placeholder="Deploy" />
+				<Error>{errors.deploy?.message}</Error>
+				<input {...register('thumbnail')} placeholder="Thumbnail" />
+				<Error>{errors.thumbnail?.message}</Error>
+				<input {...register('tags')} placeholder="Tags" />
+				<Error>{errors.tags?.message}</Error>
+				<button type="button" onClick={ () => reset() }>RESET</button>
+				<button type="submit">ADD</button>
+			</form>
+		</div>
+  );
+};
