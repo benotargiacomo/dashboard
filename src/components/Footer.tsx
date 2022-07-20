5c7803785faed1646658ca9255a94452bbfa5276
benotargiacomo <benotargiacomo@gmail.com>
2022-07-20 01:16:57 -0300

    dashboard updates

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer: React.FC = () => {
-    return (
-      <footer className="fixed bottom-0 w-full bg-white">
-        Footer
-      </footer>
-    );
-}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,63 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { trpc } from '@/utils/trpc';
+import React, { useState } from 'react';
+// import { trpc } from '@/utils/trpc';
 
-import { EditWork } from '@/components/EditWork';
+// import * as Dialog from '@radix-ui/react-dialog';
+
 import { AddWork } from '@/components/AddWork';
+import { WorkPanel } from '@/components/WorkPanel';
+import Image from 'next/image';
 
 
 const Dashboard: React.FC = () => {
   const [addIsVisible, setAddIsVisible] = useState<boolean>(false);
-  const [editIsVisible, setEditIsVisible] = useState<boolean>(false);
-  const [currentEditId, setCurrentEditId] = useState<string>('');
-  const { data, status } = trpc.useQuery(['work.getAll']);
-  const utils = trpc.useContext()
-  
-  const isActive = addIsVisible || editIsVisible;
-
-  const { mutate } = trpc.useMutation('work.delete', {
-    async onSuccess() {
-      await utils.invalidateQueries(['work.getAll']);
-    },
-  });
-  
-  const editProjectToggle = (id: string) => {
-    setCurrentEditId(id)
-
-    setEditIsVisible(true);
-  }
-
-  const deleteProject = (id: string)=> {
-    // deletar as tags se não tiver nenhum projeto usando
-    mutate({ id })
-  }
-
-  useEffect(() => {
-    for (const { id } of data ?? []) {
-      utils.prefetchQuery(['work.getById', { id }]);
-    }
-  }, [data, utils]);
 
   return (
-    <>
-      <h1>Dashboard</h1>
-      <button type="button" disabled={ isActive } onClick={ () => setAddIsVisible(true) }>ADD</button>
-      { addIsVisible && <AddWork  setAddIsVisible={  setAddIsVisible } /> }
-      { editIsVisible && <EditWork currentEditId={ currentEditId } setEditIsVisible={ setEditIsVisible } /> }
+    <div className="min-h-screen w-full flex flex-col bg-red-500">
+      <header className="w-full h-14 lg:h-20 px-6 lg:px-20 flex items-center justify-between drop-shadow-down bg-white">
+        <span className="text-lg font-bold text-blue-500 lg:text-2xl">
+          Dashboard
+        </span>
+        <nav className="flex gap-1">
+          <button
+            type="button"
+            className="flex items-center border border-slate-500 rounded-2xl hover:text-white hover:bg-slate-500 hover:border-0"
+            onClick={ () => setAddIsVisible(true) }
+          > 
+            <Image
+              src="/assets/plus-icon.svg"
+              height={24}
+              width={24}
+              alt="Deploy"
+            />
+          </button>
+          <button type="button">
+            <Image
+              src="/assets/plus-icon.svg"
+              height={24}
+              width={24}
+              alt="Deploy"
+            />
+          </button>
+        </nav>
+      </header>
       <main>
-        <li>
-          {data?.map((work) => (
-            <ul key={work.id}>
-              <div className="flex flex-col gap-2">
-                <span>{work.name}</span>
-                <span>{work.description}</span>
-                <button type='button' disabled={ isActive } onClick={() => deleteProject(work.id) }>DELETE</button>
-                <button type='button' disabled={ isActive } onClick={ () => editProjectToggle(work.id) }>EDIT</button>
-              </div>
-            </ul>
-          ))}
-        </li>
+        <div>
+          
+        {/* <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <button onClick={ () => console.log('EDITAR')}>EDITAR</button>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay>
+            <Dialog.Content className='w-2/4 h-2/4 fixed inset-y-1/2'>
+            <Dialog.Title>Edit profile</Dialog.Title>
+            <Dialog.Description>
+              Make changes to your profile here. Click save when you are done.
+            </Dialog.Description>
+            <Dialog.Close >
+              SAVE CHANGES
+            </Dialog.Close>
+            </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root> */}
+
+          { addIsVisible && <AddWork  setAddIsVisible={  setAddIsVisible } /> }
+        </div>
+        <WorkPanel />
       </main>
-    </>
+    </div>
   )
 }
 
