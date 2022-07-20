6958ba443fdaaaabe36e071278707012400c63ca
benotargiacomo <benotargiacomo@gmail.com>
2022-07-20 00:59:59 -0300

    extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./src/pages/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'half-screen': '50vh'
+      },
+      fontFamily: {
+        pop: ['Poppins']
+      },
+      dropShadow: {
+        'hard': '0.15rem 0.15rem 0.1rem rgba(0, 0, 0, 0.3)',
+        'down': '0 0.4rem 0.3rem rgba(0, 0, 0, 0.25)',
+      }
+    },
   },
   plugins: [],
 }
