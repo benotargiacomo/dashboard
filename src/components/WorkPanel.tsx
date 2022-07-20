74394fd928f963e15c08e6cbff901dcf1554fd90
benotargiacomo <benotargiacomo@gmail.com>
2022-07-20 01:03:02 -0300

    add dashboard work component

---
+++ src/components/WorkPanel.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { trpc } from '@/utils/trpc';
+
+import { EditWork } from './EditWork';
+
+export const WorkPanel: React.FC = () => {
+  const [editIsVisible, setEditIsVisible] = useState<boolean>(false);
+  const [currentEditId, setCurrentEditId] = useState<string>('');
+  const { data, status } = trpc.useQuery(['work.getAll']);
+  const utils = trpc.useContext()
+
+  const { mutate } = trpc.useMutation('work.delete', {
+    async onSuccess() {
+      await utils.invalidateQueries(['work.getAll']);
+    },
+  });
+
+  const editProjectToggle = (id: string) => {
+    setCurrentEditId(id)
+
+    setEditIsVisible(true);
+  }
+
+  const deleteProject = (id: string)=> {
+    // deletar as tags se não tiver nenhum projeto usando
+    mutate({ id })
+  }
+
+  useEffect(() => {
+    for (const { id } of data ?? []) {
+      utils.prefetchQuery(['work.getById', { id }]);
+    }
+  }, [data, utils]);
+
+    return (
+      <main className="flex flex-col">
+        { editIsVisible && <EditWork currentEditId={ currentEditId } setEditIsVisible={ setEditIsVisible } /> }
+        <li>
+        {data?.map((work) => (
+          <ul key={work.id}>
+            <section className="flex flex-col border border-blue-500">
+              <span>{work.name}</span>
+              <span>{work.description}</span>
+              <button type='button' onClick={() => deleteProject(work.id) }>DELETE</button>
+              <button type='button' onClick={ () => editProjectToggle(work.id) }>EDIT</button>
+            </section>
+          </ul>
+        ))}
+      </li>
+    </main>
+    );
+}
